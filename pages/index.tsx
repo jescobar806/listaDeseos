260d3cfbe818c5f222c35267fd645e89a723cbd6
juan escobar <jescobar806@gmail.com>
2022-03-29 15:21:20 -0500

    Se avanza en el inicio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
-
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <div>
-
+      <head>
+        <title>Home | Lista de deseos </title>
+      </head>
+      <p>Pagina de inicio</p>
+      <div>
+        <i className= 'fas fa-home'/>
+      </div>
     </div>
   )
 }

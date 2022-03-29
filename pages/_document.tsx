103f75c7ab5fe15397d090a8b32e72d2bbfdb97b
juan escobar <jescobar806@gmail.com>
2022-03-29 15:44:28 -0500

    Se soluciona error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,36 +1,25 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+/* eslint-disable @next/next/no-sync-scripts */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    //const originalRenderPage = ctx.renderPage
-
-    // Run the React rendering logic synchronously
-    //ctx.renderPage = () =>
-      //originalRenderPage({
-        // Useful for wrapping the whole react tree
-        //enhanceApp: (App) => App,
-        // Useful for wrapping in a per-page basis
-        //enhanceComponent: (Component) => Component,
-      //})
-
-    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return { ...initialProps }
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html>
-        <Head />
-            <script src="https://kit.fontawesome.com/fdca256134.js" crossOrigin="anonymous"></script>
+        <Head>
+          <script src='https://kit.fontawesome.com/fdca256134.js' crossOrigin='anonymous' />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
